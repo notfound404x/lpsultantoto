0c31d7f07b76c0bee22b12f7be6bd5e41ff1dc59
NotFound404x <buku4dofficial@gmail.com>
2025-05-23 22:54:46 +0700

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-$url = base64_decode("aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL25vdGZvdW5kNDA0eC9pbmJveHN1bHRhbnRvdG8vcmVmcy9oZWFkcy9tYWluL2xhbmRpbmdwYWdlLmh0bWw=");
-echo file_get_contents($url);
-?>
